99be8a083e880355dd12bea639b190b56d572d2c
jykim <kimorkim@gmail.com>
2020-02-08 04:33:51 +0900

    add fauna db

--- pages/map.jsx
+++ pages/map.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import MapComponnent from "../components/template/map";
 
 function Map() {
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("/api");
+      const newData = await res.json();
+      console.log(newData);
+      // setData(newData);
+    }
+    getData();
+  }, []);
   return (
     <>
       <Head>
